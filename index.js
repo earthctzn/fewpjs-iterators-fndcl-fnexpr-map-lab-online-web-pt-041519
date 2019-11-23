95324da594e05d0dbc713c442a9ac8cef3c29733
earthctzn <earthctzn1@gmail.com>
2019-11-23 15:19:16 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  
+  return tutorials.map(title => {
+    let words = title.split(' ')
+    let caps = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    return caps
+    
+  })
+
+};
